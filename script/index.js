2bd95495d3afd7dca28d26992be46b9efd09244f
manishtmtmt <manisht.mt.mt@gmail.com>
2022-04-01 22:52:23 +0530

    added signup and login

--- script/index.js
+++ script/index.js
@@ -38,7 +38,7 @@ var c3=[
     ];
 
 var grocery1=[
-    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498479/1613247367528.png?imwidth=414&impolicy=hq",name:"Big Bazar",tooltip:"Big Bazar",url:"baigbazar.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498479/1613247367528.png?imwidth=414&impolicy=hq",name:"Big Bazar",tooltip:"Big Bazar",url:"bigbazaar.html"},
     {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498482/1620737072141.png?imwidth=414&impolicy=hq",name:"Urban Ladder",tooltip:"Uraban Ladder",url:"urbanladder.html"},
     {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498480/1620737097910.jpg?imwidth=414&impolicy=hq",name:"Home",tooltip:"Home",url:"home.html"},
     {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498484/1610566898069.jpg?imwidth=414&impolicy=hq",name:"More",tooltip:"More",url:"home.html"}
