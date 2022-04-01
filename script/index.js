a85e6d1e282911da350b3e12f4bb3e200edfc4d6
manishtmtmt <manisht.mt.mt@gmail.com>
2022-04-01 11:17:46 +0530

    added home page

---
+++ script/index.js
@@ -0,0 +1,359 @@
+var  c1=[
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/710342/1620736738312.jpg?imwidth=140&impolicy=hq",name:"Central",tooltip:"Central",url:"central.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498075/1593586492547.jpg?imwidth=140&impolicy=hq",name:"Myntra",tooltip:"Myntra",url:"myntra.html"},   
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498074/1613028372599.jpg?imwidth=140&impolicy=hq",name:"Pantaloons",tooltip:"Pantaloons",url:"pantaloons.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498078/1620736180986.jpg?imwidth=140&impolicy=hq",name:"Lifestyle",tooltip:"Lifestyle",url:"lifestyle.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498076/1616184425996.png?imwidth=140&impolicy=hq",name:"Max Fashion",tooltip:"Max Fashion",url:"maxfashion.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498091/1620736830834.png?imwidth=140&impolicy=hq",name:"U.S.Polo Assn.",tooltip:"U.S.Polo Assn.",url:"uspolo.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/994845/1643780245028.png?imwidth=140&impolicy=hq",name:"Brand Factory",tooltip:"Brand Factory",url:"brandfactory.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498086/1610536486321.jpg?imwidth=140&impolicy=hq",name:"Levis",tooltip:"Levis",url:"levis.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498239/1620736478260.jpg?imwidth=140&impolicy=hq",name:"ONLY",tooltip:"ONLY",url:"only.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498254/1610536539307.jpg?imwidth=140&impolicy=hq",name:"UCB",tooltip:"UCB",url:"ucb.html"}
+];
+
+var c2=[
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498530/1610537092744.png?imwidth=140&impolicy=hq",name:"Iconic",tooltip:"Iconic",url:"iconic.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498114/1610537113386.jpg?imwidth=140&impolicy=hq",name:"Raymonds",tooltip:"Raymonds",url:"raymonds.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498115/1610537339349.jpg?imwidth=140&impolicy=hq",name:"Jockey",tooltip:"Jockey",url:"jockey.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498256/1620736733672.jpg?imwidth=140&impolicy=hq",name:"Vera Moda",tooltip:"Vera Moda",url:"veramode.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498129/1613028463583.png?imwidth=140&impolicy=hq",name:"Beverly Hills",tooltip:"Beverly Hills",url:"beverlyhills.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498127/1610537689405.png?imwidth=140&impolicy=hq",name:"Madame",tooltip:"Madame",url:"madame.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/710419/1610542731323.jpg?imwidth=140&impolicy=hq",name:"Peter England",tooltip:"Peter England",url:"peterengland.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/710420/1610543135214.jpg?imwidth=140&impolicy=hq",name:"Ven Heusen",tooltip:"Ven Heusen",url:"venheusen.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498111/1610543084890.jpg?imwidth=140&impolicy=hq",name:"Louis philippe",tooltip:"Louis philippe",url:"louisphilippe.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/710421/1610543158966.jpg?imwidth=140&impolicy=hq",name:"Planet Fashion",tooltip:"",url:"planetfashion.html"},
+];
+
+var c3=[
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/995524/1643867277967.jpg?imwidth=140&impolicy=hq",name:"Bata",tooltip:"Bata",url:"bata.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/995525/1643867496758.jpg?imwidth=140&impolicy=hq",name:"Nike",tooltip:"Nike",url:"nike.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/995526/1643867362885.png?imwidth=140&impolicy=hq",name:"Puma",tooltip:"Puma",url:"puma.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/995527/1643867532232.jpg?imwidth=140&impolicy=hq",name:"Khadim's",tooltip:"Khadim's",url:"khadim.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/995528/1643867625370.jpg?imwidth=140&impolicy=hq",name:"Red Chief",tooltip:"Red Chief",url:"redchief.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/710655/1643866806024.jpg?imwidth=140&impolicy=hq",name:"Speedo",tooltip:"Speedo",url:"speedo.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/995529/1643867506442.jpg?imwidth=140&impolicy=hq",name:"Sports station",tooltip:"Sports station",url:"sportsstation.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/995530/1643867664954.jpg?imwidth=140&impolicy=hq",name:"Woodland",tooltip:"Woodland",url:"woodland.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/710654/1643866837835.jpg?imwidth=140&impolicy=hq",name:"Metro Shoes",tooltip:"Metro Shoes",url:"metroshoes.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/710657/1613028563419.jpg?imwidth=140&impolicy=hq",name:"Mochi",tooltip:"Mochi",url:"mochi.html"}, 
+    ];
+
+var grocery1=[
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498479/1613247367528.png?imwidth=414&impolicy=hq",name:"Big Bazar",tooltip:"Big Bazar",url:"baigbazar.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498482/1620737072141.png?imwidth=414&impolicy=hq",name:"Urban Ladder",tooltip:"Uraban Ladder",url:"urbanladder.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498480/1620737097910.jpg?imwidth=414&impolicy=hq",name:"Home",tooltip:"Home",url:"home.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498484/1610566898069.jpg?imwidth=414&impolicy=hq",name:"More",tooltip:"More",url:"home.html"}
+    ];
+
+var grocery2 = [
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498485/1620737113562.png?imwidth=414&impolicy=hq",name:"Home centre",tooltip:"Home centre",url:"homecentre.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498490/1610567023040.png?imwidth=414&impolicy=hq",name:"Prestige",tooltip:"Prestige",url:"prestige.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498486/1610567208285.png?imwidth=414&impolicy=hq",name:"Ratnadeep",tooltip:"Ratnadeep",url:"ratnadeep.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/498486/1610567208285.png?imwidth=414&impolicy=hq",name:"Ratnadeep",tooltip:"Ratnadeep",url:"ratnadeep.html"}
+]
+
+var jewellery=[
+
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/711192/1613028659915.jpg?imwidth=414&impolicy=hq",name:"Kasturi | Up to 40% Cashback",tooltip:"Kasturi | Up to 40% Cashback",url:"kasturi.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/711194/1620745874093.jpg?imwidth=414&impolicy=hq",name:"Jewelfina",tooltip:"Jewelfina",url:"jewelfina.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/711199/1613028660058.jpg?imwidth=414&impolicy=hq",name:"Tanishq",tooltip:"Tanishq",url:"tanishq.html"},
+    {img:"https://assetscdn1.paytm.com/images/catalog/view_item/711200/1620745456365.jpg?imwidth=414&impolicy=hq",name:"Upto 16% cashback",tooltip:"Upto 16% cashback",url:"Josalukkas.html"}    
+    ]
+c1.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c1s1").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+})
+
+c1.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c1s2").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+});
+
+c1.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c1s3").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+});
+
+c2.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c2s1").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+})
+
+c2.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c2s2").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+})
+
+c2.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c2s3").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+})
+
+c3.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c3s1").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+})
+
+c3.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c3s2").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+})
+
+c3.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c3s3").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+})
+
+grocery1.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card2");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c4s1").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+})
+
+grocery2.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card2");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c4s2").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+});
+
+jewellery.map(function(el){
+    var cards = document.createElement("div")
+    cards.setAttribute("class","card2");
+    cards.setAttribute("title",el.tooltip);
+    document.querySelector("#c5s1").append(cards);
+
+    cards.addEventListener("click", myFun);
+    function myFun() {
+        window.location.href = el.url;
+    };
+
+    var div1 = document.createElement("div")
+    var div2 = document.createElement("div")
+
+    var image = document.createElement("img")
+    image.src = el.img;
+
+    var names = document.createElement("p")
+    names.innerText = el.name;
+
+    div1.append(image);
+    div2.append(names);
+    cards.append(div1,div2)
+});
